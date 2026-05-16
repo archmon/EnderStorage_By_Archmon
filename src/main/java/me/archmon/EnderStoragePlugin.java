294f6f947f7eaaf4a89ca33e331114aa68160f01
archmon <archmon_2007@yahoo.com>
2026-05-16 17:18:28 -0500

    Build works

--- src/main/java/me/archmon/EnderStoragePlugin.java
+++ src/main/java/me/archmon/EnderStoragePlugin.java
@@ -6,6 +6,7 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.hypixel.hytale.server.core.command.system.CommandRegistry;
+import com.hypixel.hytale.server.core.event.events.player.PlayerInteractEvent;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import me.archmon.commands.EnderStorageModVersion;
@@ -48,10 +49,10 @@ public class EnderStoragePlugin extends JavaPlugin {
         EnderStorageTickSystem enderStorageTickSystem = new EnderStorageTickSystem(manager);
         manager.setTickSystem(enderStorageTickSystem);
         this.getEntityStoreRegistry().registerSystem(enderStorageTickSystem);
-        //this.getEntityStoreRegistry().registerSystem(new EnderStorageBreakSystem(manager));
-        //this.getEntityStoreRegistry().registerSystem(new EnderStorageUseBlockSystem(manager));
+        this.getEntityStoreRegistry().registerSystem(new EnderStorageBreakSystem(manager));
+        this.getEntityStoreRegistry().registerSystem(new EnderStorageUseBlockSystem(manager));
         //warning PlayerInteractEvent is deprecated
-        //this.getEventRegistry().registerGlobal(PlayerInteractEvent.class, new EnderStorageListener(manager));
+        this.getEventRegistry().registerGlobal(PlayerInteractEvent.class, new EnderStorageListener(manager));
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             if (manager != null) {
                 manager.saveAll();
