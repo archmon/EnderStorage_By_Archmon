f5b7b800708dd92f2a2a89639ff7b16fa98e0093
archmon <archmon_2007@yahoo.com>
2026-05-22 18:49:36 -0500

    Ready for release 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "me.archmon"
-version = "0.1.3"
+version = "1.0.0"
 val javaVersion = 25
 
 repositories {
--- src/main/java/me/archmon/commands/VoidStorageBugReport.java
+++ src/main/java/me/archmon/commands/VoidStorageBugReport.java
@@ -15,7 +15,7 @@ public class VoidStorageBugReport extends CommandBase {
 
     @Override
     protected void executeSync(@NonNull CommandContext commandContext) {
-        String website = "opps, remind me to add the link";
+        String website = "https://github.com/archmon/VoidStorage_By_Archmon/issues";
         commandContext.sendMessage(Message.raw("Bug reports for the VoidStorage Mod by Archmon can be reported here :" + website));
     }
 }
